99b7b7be4c943700a4bf381180d3a23904d1ca8f
acharayaP03 <acharyaP03@gmail.com>
2024-12-08 20:10:27 +1100

    useAuthFormSchema

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { z } from 'zod';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function useAuthFormSchema<T>(formType: T) {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === 'register'
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
 }
