6e3cd980f25fc785458a74e0ab4df4e1a91c57ef
acharayaP03 <acharyaP03@gmail.com>
2024-12-23 18:36:39 +1100

    user server action for signing up

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,72 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { parseStringify } from "../utils";
+
+/**
+ * 1. User enters full name and email
+ * 2. Check if the user already exist using the email address
+ * 3. Send OTP  to user's email (this will send a secret key for creating session. This key will be used to create a session)
+ * 4. Create a new user document if hte user is a newe user.
+ * 5. Return the user's accountId that will be used to complete the login.
+ * 6. Verify OTP and authenticate to login.
+ */
+
+const getUserByEmail = async (email: string) => {
+  const { databases } = await createAdminClient();
+
+  const result = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.userCollectionId,
+    [Query.equal("email", [email])],
+  );
+
+  return result.total > 0 ? result.documents[0] : null;
+};
+
+const handleError = (error: unknown, message: string) => {
+  console.error(error);
+  throw new Error(message);
+};
+
+const sendEmailOTP = async ({ email }: { email: string }) => {
+  const { account } = await createAdminClient();
+
+  try {
+    const session = await account.createEmailToken(ID.unique(), email);
+    return session.userId;
+  } catch (error) {
+    handleError(error, "Failed to send OTP");
+  }
+};
+
+export const createAccount = async ({
+  fullName,
+  email,
+}: {
+  fullName: string;
+  email: string;
+}) => {
+  const existingUser = await getUserByEmail(email);
+
+  const accountId = await sendEmailOTP({ email });
+  if (!existingUser) {
+    const { databases } = await createAdminClient();
+
+    await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        fullName,
+        email,
+        accountId,
+        avatar: "https://i.pravatar.cc/50",
+      },
+    );
+  }
+
+  return parseStringify({ accountId });
+};
