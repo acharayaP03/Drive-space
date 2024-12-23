bd8a62184bcc21cb0c3fc61524c00b169415fe0e
acharayaP03 <acharyaP03@gmail.com>
2024-12-23 18:37:25 +1100

    manual test if signup works

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,47 +1,62 @@
-'use client';
-import { useState } from 'react';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+"use client";
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import { useAuthFormSchema } from '@/lib/utils';
+import { useAuthFormSchema } from "@/lib/utils";
+import { createAccount } from "@/lib/actions/user.actions";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
-import { Form } from '@/components/ui/form';
-import FormField from './FormFields/FormField';
+import Image from "next/image";
+import Link from "next/link";
+import FormField from "./FormFields/FormField";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
 
-type FormType = 'login' | 'register';
+type FormType = "login" | "register";
 
 export default function AuthForm({ type }: { type: FormType }) {
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState("");
 
   const formSchema = useAuthFormSchema(type);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      fullName: '',
-      email: '',
+      fullName: "",
+      email: "",
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log('Value: ', values);
-
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const user = await createAccount({
+        fullName: values.fullName,
+        email: values.email,
+      });
+
+      setAccountId(user.accountId);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage("Failed to create account. Please try again later.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
   }
   return (
     <>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
           <h1 className="form-title">
-            {type === 'login' ? 'Login' : 'Register new account'}
+            {type === "login" ? "Login" : "Register new account"}
           </h1>
 
-          {type === 'register' && (
+          {type === "register" && (
             <FormField
               form={form}
               name="fullName"
@@ -62,7 +77,7 @@ export default function AuthForm({ type }: { type: FormType }) {
             className="form-submit-button"
             disabled={isLoading}
           >
-            {type === 'login' ? 'Sign In' : 'Create new account'}
+            {type === "login" ? "Sign In" : "Create new account"}
 
             {isLoading && (
               <Image
@@ -79,16 +94,16 @@ export default function AuthForm({ type }: { type: FormType }) {
 
           <div className="body-2 flex justify-center">
             <p className="text-light-100">
-              {type === 'login'
+              {type === "login"
                 ? "Don't have an account?"
-                : 'Already have an account?'}
+                : "Already have an account?"}
             </p>
             <Link
-              href={type === 'login' ? '/register' : '/login'}
+              href={type === "login" ? "/register" : "/login"}
               className="ml-1 font-medium text-brand"
             >
-              {' '}
-              {type === 'login' ? 'Create new account' : 'Login'}
+              {" "}
+              {type === "login" ? "Create new account" : "Login"}
             </Link>
           </div>
         </form>
