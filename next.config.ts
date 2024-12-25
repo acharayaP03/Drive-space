2c48ab482af2a2218960654e88b67799a354311b
acharayaP03 <acharyaP03@gmail.com>
2024-12-25 23:03:19 +1100

    configure remote patterns for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+      {
+        protocol: "https",
+        hostname: "https://i.pravatar.cc/50",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
