d474863bba85864e12708a808ce82dd66230be35
acharayaP03 <acharyaP03@gmail.com>
2024-12-25 23:03:41 +1100

    root layout for signed in users

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,27 @@
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
+import { getCurrentLoggedInUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const currentUser = await getCurrentLoggedInUser();
+
+  if (!currentUser) {
+    return redirect("/login");
+  }
+
   return (
     <main className="flex h-screen">
-      Sidebar
+      <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        Mobile navigation Header
+        <MobileNavigation />
+        <Header />
         <div className="main-content">{children}</div>
       </section>
     </main>
