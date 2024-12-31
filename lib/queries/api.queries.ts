a6e8b43a9e800f20e6ad2f1517f5e5e6b47b8eff
acharayaP03 <acharyaP03@gmail.com>
2024-12-31 14:46:14 +1100

    add search, sort and limit query

--- lib/queries/api.queries.ts
+++ lib/queries/api.queries.ts
@@ -1,6 +1,12 @@
 import { Models, Query } from "node-appwrite";
 
-export function createQueries(currentUser: Models.Document) {
+export function createQueries(
+    currentUser: Models.Document,
+    types: string[],
+    searchText: string,
+    sort: string,
+    limit?: number,
+) {
     const queries = [
         Query.or([
             Query.equal("owner", [currentUser.$id]),
@@ -8,7 +14,26 @@ export function createQueries(currentUser: Models.Document) {
         ]),
     ];
 
-    // Todo: Search sort limit
+    if (types.length > 0) {
+        queries.push(Query.equal("type", types));
+    }
+    if (searchText) {
+        queries.push(Query.contains("name", [searchText]));
+    }
+
+    if (limit) {
+        queries.push(Query.limit(limit));
+    }
+
+    if (sort) {
+        const [sortBy, orderBy] = sort.split("-");
+
+        queries.push(
+            orderBy === "asc"
+                ? Query.orderAsc(sortBy)
+                : Query.orderDesc(sortBy),
+        );
+    }
 
     return queries;
 }
