ca7c1e379a42f2cfd971948f33c675f505b34777
acharayaP03 <acharyaP03@gmail.com>
2024-12-31 18:23:21 +1100

    dashboard components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,95 @@
-export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold text-brand">
-        Drive_it - The only free storage solution you will be needing it.
-      </h1>
-    </div>
-  );
+import ActionMenu from "@/components/ActionMenu";
+import FormattedDateTime from "@/components/FormattedDateTime";
+import Thumbnail from "@/components/Thumbnail";
+import { getFiles, getTotalSpaceUsedSummary } from "@/lib/actions/file.actions";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
+import { Separator } from "@radix-ui/react-separator";
+import Image from "next/image";
+import Link from "next/link";
+import { Models } from "node-appwrite";
+
+export default async function Home() {
+    const [files, totalSpace] = await Promise.all([
+        getFiles({ types: [], limit: 10 }),
+        getTotalSpaceUsedSummary(),
+    ]);
+    const usageSummary = getUsageSummary(totalSpace);
+    return (
+        <div className="dashboard-container">
+            <section>
+                <ul className="dashboard-summary-list">
+                    {usageSummary.map((summary) => (
+                        <Link
+                            href={summary.url}
+                            key={summary.title}
+                            className="dashboard-summary-card"
+                        >
+                            <div className="space-y-4">
+                                <div className="flex justify-between gap-3">
+                                    <Image
+                                        src={summary.icon}
+                                        width={100}
+                                        height={100}
+                                        alt="uploaded image"
+                                        className="summary-type-icon"
+                                    />
+                                    <h4 className="summary-type-size">
+                                        {convertFileSize(summary.size) || 0}
+                                    </h4>
+                                </div>
+
+                                <h5 className="summary-type-title">
+                                    {summary.title}
+                                </h5>
+                                <Separator className="bg-light-400" />
+                                <FormattedDateTime
+                                    date={summary.latestDate}
+                                    className="text-center"
+                                />
+                            </div>
+                        </Link>
+                    ))}
+                </ul>
+            </section>
+            <section className="dashboard-recent-files">
+                <h2 className="h3 xl:h2 text-light-100">
+                    Recent files uploaded
+                </h2>
+
+                {files.documents.length > 0 ? (
+                    <ul className="mt-5 flex flex-col gap-4">
+                        {files.documents.map((file: Models.Document) => (
+                            <Link
+                                href={file.url}
+                                target="_blank"
+                                className="flex items-center gap-3"
+                                key={file.$id}
+                            >
+                                <Thumbnail
+                                    type={file.type}
+                                    extension={file.extension}
+                                    url={file.url}
+                                />
+
+                                <div className="recent-file-details">
+                                    <div className="flex flex-col gap-1">
+                                        <p className="recent-file-name">
+                                            {file.name}
+                                        </p>
+                                        <FormattedDateTime
+                                            date={file.$createdAt}
+                                            className="caption"
+                                        />
+                                    </div>
+                                    <ActionMenu file={file} />
+                                </div>
+                            </Link>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="empty-list">No files uploaded yet</p>
+                )}
+            </section>
+        </div>
+    );
 }
